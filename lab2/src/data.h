e5235316f0a63e67bee3d9ca4288766f74894aff
Alesia Yarmash <159628513+lesik248@users.noreply.github.com>
2025-04-18 23:18:28 +0300

    Delete lab2 directory

--- lab2/src/data.h
+++
@@ -1,8 +0,0 @@
-git //
-// Created by alesy on 11.03.2025.
-//
-
-#ifndef DATA_H
-#define DATA_H
-
-#endif //DATA_H
--- lab2/src/functions.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by alesy on 11.03.2025.
-//
-
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-#endif //FUNCTIONS_H
